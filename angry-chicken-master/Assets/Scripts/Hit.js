e0acad7fe1f62d57da8d3c310681ff61f9d1f10a
sachanan <sachin@sachin-no-MacBook-Air.local>
2014-04-11 17:42:50 +0900

    初コミット
    記念すべき初コミット

---
+++ angry-chicken-master/Assets/Scripts/Hit.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var limit : float = 4.0f;
+var oldValue : float = 0.0f;
+
+var fx : GameObject;
+
+function OnCollisionStay( collisionInfo : Collision )
+{
+	var diff : float = Mathf.Abs(oldValue - collisionInfo.relativeVelocity.magnitude);
+	
+	if ( diff > limit ){
+	Instantiate(fx,
+				this.transform.position,
+				this.transform.rotation);	
+				Destroy( this.gameObject );
+	}
+	oldValue = collisionInfo.relativeVelocity.magnitude;
+	
+}
